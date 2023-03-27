7c31310366356d20d74bbc02941618935e1ddad5
Kayuhakirito <125715001+Kayuhakirito@users.noreply.github.com>
2023-03-27 20:55:06 +0700

    Add files via upload

---
+++ lap7/XeMayLeoNui.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lap7;
+
+/**
+ *
+ * @author kayuh
+ */
+public class XeMayLeoNui extends XeMay {
+
+    @Override
+    void brake() {
+        System.out.println("Brake xe may leo nui");
+    }
+    
+}
---
+++ lap7/main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lap7;
+
+/**
+ *
+ * @author kayuh
+ */
+public class main {
+    public static void main(String[] args) {
+        XeMayLeoNui ln = new XeMayLeoNui();
+        XeMayTheThao tt = new XeMayTheThao();
+        ln.brake();
+        tt.brake();
+    }
+}
